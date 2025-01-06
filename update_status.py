734c2781e268b7597f812c8f66bc018c68d270f5
SirWiliam <cloud64region@mail.ru>
2025-01-06 19:25:45 +0400

    Grade 1. Этап 2: Задание 2

--- update_status.py
+++ update_status.py
@@ -1,8 +1,10 @@
 # Создаём словарь со статусами заметок
-status_list = {'1': "Выполняется",
+status_list = {
+               '1': "Выполняется",
                '2': "Выполнена",
                '3': "Отложена",
-               '4': "Просрочена"}
+               '4': "Просрочена"
+               }
 # Начало работы программы
 print('Статус вашей заметки: ', 'Выполняется')
 
@@ -14,43 +16,25 @@ while True:
     if status == '1':
         print('Ваш выбор: 1')
         print('Обновлённый статус заметки:', "Выполняется")
-        update_status = input('Для изменения статуса введите - "Поменять"\n'
-                          'Для отмены изменения нажмите - Enter: ')
-        if update_status == 'Поменять':
-            continue
-        elif update_status == '':
-            break
-    if status == '2':
+    elif status == '2':
         print('Ваш выбор: 2')
         print('Обновлённый статус заметки:', "Выполнена")
-        update_status = input('Для изменения статуса введите - "Поменять"\n'
-                            'Для отмены изменения нажмите - Enter: ')
-        if update_status == 'Поменять':
-          continue
-        elif update_status == '':
-          break
-    if status == '3':
+    elif status == '3':
         print('Ваш выбор: 3')
         print('Обновлённый статус заметки:', "Отложена")
-        update_status = input('Для изменения статуса введите - "Поменять"\n'
-                            'Для отмены изменения нажмите - Enter: ')
-        if update_status == 'Поменять':
-            continue
-        elif update_status == '':
-            break
-    if status == '4':
+    elif status == '4':
         print('Ваш выбор: 4')
         print('Обновлённый статус заметки:', "Просрочена")
-        update_status = input('Для изменения статуса введите - "Поменять"\n'
-                            'Для отмены изменения нажмите - Enter: ')
-        if update_status == 'Поменять':
-            continue
-        elif update_status == '':
-            break
     else:
         print('Не верный ввод!')
         continue
-
-# Предлагаем пользователю изменить статус
+# Спрашиваем пользователя об изменении выбора
+    update_status = input('Для изменения статуса введите любой символ\n'
+                          'Для отмены изменения нажмите - Enter: ')
+    if update_status == type(str):
+        continue
+    elif update_status == '':
+        break
+# Выводим подтверждённый статус
 print('Статус вашей заметки: ', status_list.get(status))
 
