d28be15802f98cb8befb5fdb4250c37289202c7a
SirWiliam <cloud64region@mail.ru>
2025-01-06 19:55:46 +0400

    Grade 1. Этап 2: Задание 4

--- multiple_notes.py
+++ multiple_notes.py
@@ -1,4 +1,3 @@
-# Программа запускается с приветствия
 from time import sleep
 from datetime import datetime
 note_list = []
@@ -19,64 +18,103 @@ i = 0
 while True:
     print(f'Пожалуйста введите данные для заметки №{i + 1}')
     note = {}
-    username = input('Введите ваше имя: ')
-    note['Имя:'] = username
-    if username == '':
-        print('Неверный ввод!')
-        continue
-    title = input('Введите заголовок вашей заметки: ')
-    note['Заголовок:'] = title
-    if title == '':
-        print('Неверный ввод!')
-        continue
-    elif title == note.values():
-        print('Вы ввели похожий заголовок!')
-        title_change = input('Хотите его заменить?(да/нет): ').lower()
-        if title_change == 'да':
+    while True:
+        username = input('Введите ваше имя: ')
+        note['Имя:'] = username
+        if username == '':
+            print('Неверный ввод!')
             continue
-        elif title_change == 'нет':
-            break
         else:
-            print('Неверный ввод! Попробуйте ещё раз.')
+            break
+    while True:
+        title = input('Введите заголовок вашей заметки: ')
+        note['Заголовок:'] = title
+        if title == '':
+            print('Неверный ввод!')
             continue
-    content = input('Придумайте описание вашей заметки: ')
-    note['Описание:'] = content
-    if content == '':
-        print('Неверный ввод!')
-        continue
-    status = input('Введите статус заметки (Например: "Выполняется" или Выполнена"): ')
-    note['Статус заметки:'] = status
-    if status == '':
-        print('Неверный ввод!')
-        continue
-    created_date = input('Введите дату создания заметки\n'
-      'в формате "дд-мм-гггг"(через дефис и без пробелов): ')
-    try:
-        created_date = datetime.strptime(created_date, '%d-%m-%Y').date()
-    except ValueError:
-        print('Неверный ввод! Попробуйте ещё.')
-        continue
-    note['Дата создания:'] = created_date
-    issue_date = input('Введите дату истечения заметки\n'
-      ' в формате "дд-мм-гггг"(через дефис и без пробелов): ')
-    try:
-        issue_date = datetime.strptime(issue_date, '%d-%m-%Y').date()
-    except ValueError:
-        print('Неверный ввод! Попробуйте ещё.')
-        continue
-    if issue_date == current_date:
-        print('Внимание! Дедлайн истекает сегодня.')
-        break
-    elif issue_date < current_date:
-        difference = current_date - issue_date
-        print('Внимание! Дедлайн истёк: ', difference.days, 'дней назад!')
-        break
-    elif issue_date > current_date:
-        difference_1 = issue_date - current_date
-        print('Внимание! До дедлайна: ', difference_1.days, 'дней!')
-        break
-    note['Дата дедлайна:'] = issue_date
-    print('Спасибо за ваши данные!')
+        elif title == note.values():
+            print('Вы ввели похожий заголовок!')
+            title_change = input('Хотите его заменить?(да/нет): ').lower()
+            if title_change == 'да':
+                continue
+            elif title_change == 'нет':
+                break
+            else:
+                print('Неверный ввод! Попробуйте ещё раз.')
+                continue
+        else:
+            break
+    while True:
+        content = input('Придумайте описание вашей заметки: ')
+        note['Описание:'] = content
+        if content == '':
+            print('Неверный ввод!')
+            continue
+        else:
+            break
+    while True:
+        status_list = {
+            '1': "Выполняется",
+            '2': "Выполнена",
+            '3': "Отложена",
+            '4': "Просрочена"
+             }
+        print('Пожалуйста, выберите новый статус вашей заметки')
+        print(*status_list.items(), sep='\n')
+        status = input('Введите номер статуса который вам подходит: ')
+        if status == '1':
+            print('Ваш выбор: 1')
+            print('Обновлённый статус заметки:', "Выполняется")
+        elif status == '2':
+            print('Ваш выбор: 2')
+            print('Обновлённый статус заметки:', "Выполнена")
+        elif status == '3':
+            print('Ваш выбор: 3')
+            print('Обновлённый статус заметки:', "Отложена")
+        elif status == '4':
+            print('Ваш выбор: 4')
+            print('Обновлённый статус заметки:', "Просрочена")
+        else:
+            print('Не верный ввод!')
+            continue
+        update_status = input('Для изменения статуса введите любой символ\n'
+                                  'Для отмены изменения нажмите - Enter: ')
+        if update_status == type(str):
+            continue
+        elif update_status == '':
+            note['Статус заметки:'] = status_list.get(status)
+            break
+    while True:
+        created_date = input('Введите дату создания заметки\n'
+                     'в формате "дд-мм-гггг"(через дефис и без пробелов): ')
+        try:
+            created_date = datetime.strptime(created_date, '%d-%m-%Y').date()
+            note['Дата создания:'] = created_date.strftime('%d-%m-%Y')
+            break
+        except ValueError:
+            print('Неверный ввод! Попробуйте ещё.')
+            continue
+    while True:
+        issue_date = input('Введите дату истечения заметки\n'
+                     ' в формате "дд-мм-гггг"(через дефис и без пробелов): ')
+        try:
+            issue_date = datetime.strptime(issue_date, '%d-%m-%Y').date()
+            note['Дата дедлайна:'] = issue_date.strftime('%d-%m-%Y')
+        except ValueError:
+            print('Неверный ввод! Попробуйте ещё.')
+            continue
+        if issue_date == current_date:
+            print('Внимание! Дедлайн истекает сегодня.')
+            break
+        elif issue_date < current_date:
+            difference = current_date - issue_date
+            print('Внимание! Дедлайн истёк: ', difference.days, 'дней назад!')
+            break
+        elif issue_date > current_date:
+            difference_1 = issue_date - current_date
+            print('Внимание! До дедлайна: ', difference_1.days, 'дней!')
+            break
+    print('Создание заметки завершено.')
     note_list.append(note)
     i += 1
     question = input('Хотите создать ещё одну заметку? (да/нет):').lower()
