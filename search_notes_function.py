e41b9132ab3ef80cadc9c8cbbc7011d5f6928e1f
SirWiliam <cloud64region@mail.ru>
2025-01-13 22:02:48 +0400

    Grade 1. Этап 3: Задание 4

--- search_notes_function.py
+++ search_notes_function.py
@@ -1,62 +1,86 @@
-
-notes = [{
-        'username': 'Алексей',
+# Заметки для поиска
+notes = [
+    {
+        'username': 'Алекс',
         'title': 'Список покупок',
-        'content': 'Купить продукты на неделю',
+        'content': 'Купить продукты',
         'status': 'новая',
-        'created_date': '27-11-2024',
-        'issue_date': '30-11-2024'
+        'created_date': '20-01-2025',
+        'issue_date': '30-01-2025'
     },
-        {
-            'username': 'Мария',
-            'title': 'Учеба',
-            'content': 'Подготовиться к экзамену',
-            'status': 'в процессе',
-            'created_date': '25-11-2024',
-            'issue_date': '01-12-2024'
-        },
-        {
-            'username': 'Иван',
-            'title': 'План работы',
-            'content': 'Завершить проект',
-            'status': 'выполнено',
-            'created_date': '20-11-2024',
-            'issue_date': '26-11-2024'
-        }]
+    {
+        'username': 'Дима',
+        'title': 'Спортзал',
+        'content': 'Сходить в спортзал',
+        'status': 'в процессе',
+        'created_date': '20-01-2025',
+        'issue_date': '01-02-2025'
+    },
+    {
+        'username': 'Даша',
+        'title': 'День рождения',
+        'content': 'Купить подарок',
+        'status': 'выполнено',
+        'created_date': '20-03-2025',
+        'issue_date': '26-05-2025'
+}
+]
 
 
 
-def search_notes(notes, keyword = None, status = None):
-    for i in notes:
-        if (i['title'] == keyword or
-                i['username'] == keyword or
-                i['content'] == keyword):
-            print(*i.items(), sep='\n')
-            break
 
-    #a = 0
-    #while True:
-        #for i in range(len(notes)):
-            #if (notes[i]['title'] != keyword and
-                #notes[i]['username'] != keyword and
-                #notes[i]['content'] != keyword):
-                #continue
-            #if notes[i]['status'] != status:
-                #continue
-            #elif notes[i]['status'] == status:
-                #print(f'Заметка №{a + 1}:')
-                #a += 1
-                #print(*notes[i].items(), sep='\n')
-                #print('-----------------------------')
-            #elif (notes[i]['title'] == keyword or
-                  #notes[i]['username'] == keyword or
-                  #notes[i]['content'] == keyword):
-                #print(f'Заметка №{a + 1}:')
-                #a += 1
-                #print(*notes[i].items(), sep='\n')
-                #print('-----------------------------')
-        #else:
-            #break
+def search_notes(notes, keyword=None, status=None):
+# Проверка для пустого списка
+    if len(notes) == 0:
+         print('Список заметок пуст.')
+         return []
+
+# Если не задано ключевое слово или статус вернуть исходный список заметок
+    if keyword is None and status is None:
+         return notes
+
+    notes_list = []
+
+    for note in notes:
+        my_keyword = True
+        my_status = True
+
+# Поиск по ключевому слову
+        if keyword is not None:
+            keyword = keyword.lower()
+
+# Поиск в имени пользователя, заголовке заметки или в описании заметки
+            my_keyword = (
+                    keyword in note['title'].lower() or
+                    keyword in note['content'].lower() or
+                    keyword in note['username'].lower()
+            )
+
+# Поиск по статусу
+        if status is not None:
+            my_status = note['status'].lower() == status.lower()
+
+# Поиск по двум параметрам keyword и status
+        if my_keyword and my_status:
+            notes_list.append(note)
+
+# Отобразить результат
+    if len(notes_list) > 0:
+        print('Результат поиска:')
+        for i, note in enumerate(notes_list, 1):
+            print(f'Заметка №{i}:')
+            print(f'Имя пользователя: {note['username']}')
+            print(f'Заголовок: {note['title']}')
+            print(f'Описание: {note['content']}')
+            print(f'Статус: {note['status']}')
+            print(f'Дата создания: {note['created_date']}')
+            print(f'Дата дедлайна: {note['issue_date']}')
+            if i < len(notes_list):
+                print('')
+    else:
+        print('Ваш запрос не дал результата.')
+
+    return notes_list
 
 
 while True:
@@ -66,19 +90,19 @@ while True:
                      '3 - по ключевому слову и статусу\n'
                      'Введите цифру вашего выбора: ')
     if question == '1':
-        keyword = input('Введите имя, название или описание заметки для поиска: ')
+        keyword = str(input('Введите имя, название или описание заметки для поиска: '))
         print('Результат поиска по ключевому слову:')
-        search_notes(notes, keyword)
+        keyword = search_notes(notes, keyword)
         break
     if question == '2':
         status = input('Введите статус заметки для поиска (новая, в процессе, выполнено): ')
         print('Результат поиска по статусу:')
-        search_notes(notes, status)
+        status = search_notes(notes, status)
         break
     if question == '3':
-        keyword = str(input('Введите имя, название или описание заметки для поиска: '))
-        status = str(input('Введите статус заметки для поиска (новая, в процессе, выполнена): '))
-        search_notes(notes, keyword, status)
+        keyword = input('Введите имя, название или описание заметки для поиска: ')
+        status = input('Введите статус заметки для поиска (новая, в процессе, выполнена): ')
+        keyword, status = search_notes(notes, keyword, status)
         break
 
 
