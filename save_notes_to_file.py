6ebba369339c627cd812f55f286bbae5e7d2523a
SirWiliam <cloud64region@mail.ru>
2025-01-17 19:42:42 +0400

    Grade 1. Этап 4: Задание 1

--- save_notes_to_file.py
+++ save_notes_to_file.py
@@ -1,28 +1,49 @@
-filename = open('filename.txt', 'a+', encoding='utf-8')
+import json
+# Создаём файл если его нет и закрываем.
+filename = open('filename.json', 'a+', encoding='utf-8')
 filename.close()
-notes = []
-username = input('Введите имя пользователя: ').strip().capitalize()
-title = input('Введите заголовок заметки: ').strip().capitalize()
-content = input('Введите описание заметки: ').strip().capitalize()
-status = input('Ведите статус заметки: ').strip().lower()
-created_date = input('Введите дату создания заметки:')
-issue_date = input('Введите дату дедлайна: ')
-notes.append(username)
-notes.append(title)
-notes.append(content)
-notes.append(status)
-notes.append(created_date)
-notes.append(issue_date)
 
+# Список заметок который надо записать
+notes = [
+    {
+        'username': 'Алекс',
+        'title': 'Список покупок',
+        'content': 'Закупить продукты',
+        'status': 'новая',
+        'created_date': '20-01-2025',
+        'issue_date': '30-01-2025'
+    },
+    {
+        'username': 'Дима',
+        'title': 'Спортзал',
+        'content': 'Сходить в спортзал',
+        'status': 'в процессе',
+        'created_date': '20-01-2025',
+        'issue_date': '01-02-2025'
+    },
+    {
+        'username': 'Даша',
+        'title': 'День рождения',
+        'content': 'Купить подарок',
+        'status': 'выполнено',
+        'created_date': '20-03-2025',
+        'issue_date': '26-05-2025'
+}
+]
+
+# Функция которая принимает список заметок и записывает их в файл
 def save_notes_to_file(notes, filename):
-    filename = open('filename.txt', 'w', encoding='utf-8')
-    filename.write(f"Имя пользователя: {notes[0]}\n")
-    filename.write(f"Заголовок: {notes[1]}\n")
-    filename.write(f"Описание: {notes[2]}\n")
-    filename.write(f"Статус: {notes[3]}\n")
-    filename.write(f"Дата создания: {notes[4]}\n")
-    filename.write(f"Дедлайн: {notes[5]}\n")
-    filename.close()
-    return filename
 
+    # Открываем созданный файл в формате json с функцией записи
+    filename = open('filename.json', 'w', encoding='utf-8')
+
+    # С помощью метода .dump записываем список в файл
+    with open('filename.json', 'w', encoding='utf-8') as filename:
+        json.dump(notes, filename, ensure_ascii=False, indent=4)
+
+    # Закрываем файл после записи и возвращаем из функции
+        filename.close()
+        return filename
+
+# Передаём список в функцию и вызываем её
 notes = save_notes_to_file(notes, filename)
