d07abfbf2c63616239c55ddd900e0ddb3b7f5ae5
SirWiliam <cloud64region@mail.ru>
2025-01-19 21:08:47 +0400

    Grade 1. Этап 4: Задание 2

--- load_notes_from_file.py
+++ load_notes_from_file.py
@@ -2,33 +2,31 @@
 
 file = open('filename.txt', encoding='utf-8')
 file.close()
-
+notes = []
+# Создаём функцию для чтения файла и переноса содержимого в список словарей
 def load_notes_from_file(filename):
-    notes = []
-    note = {}
 
+    # Открываем нужный файл с помощью функции with
     with open('filename.txt', 'r', encoding='utf-8') as file:
         all_string = file.readlines()
         symbols = ('— — —\n')
-        while symbols in all_string:
-            all_string.remove(symbols)
-        print(all_string) # Проверка избавления от ненужных символов
+
+        # Убираем ненужные символы за один проход
+        all_string = [line for line in all_string if line != symbols]
+
+        # Назначаем ключ и значение с помощью разделения ":", записываем в словарь и добавляем в список
         for i in all_string:
+            note = {}
             key, value = i.split(':')
             value = value.strip()
             note[key] = value
-        notes.append(note) # Передвинул и теперь только последнюю заметку записывает как надо по заданию
+            notes.append(note)
 
-        print(notes)
+        # Возвращаем из функции список словарей
+        return notes
 
-
-
-
-
-
-
-
-
-
-
+# Вызываем функцию
 load_notes_from_file(file)
+
+# Проверяем результат
+print(notes)
