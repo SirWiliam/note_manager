e0efa3a7764bcdc241152efb195ededf5b361a04
SirWiliam <cloud64region@mail.ru>
2025-01-20 19:27:31 +0400

    Grade 1. Этап 4: Задание 2

--- load_notes_from_file.py
+++ load_notes_from_file.py
@@ -1,13 +1,12 @@
 
 
-
 notes = []
 # Создаём функцию для чтения файла и переноса содержимого в список словарей
 def load_notes_from_file(filename):
 
-    # Открываем нужный файл с помощью функции with
-    with open('filename.txt', 'r', encoding='utf-8') as file:
-        all_string = file.readlines()
+    # Открываем нужный файл с помощью конструкции with
+    with open('filename.txt', 'r', encoding='utf-8') as filename:
+        all_string = filename.readlines()
         if len(all_string) == 0:
             print('Открытый файл пуст')
             return notes
@@ -24,20 +23,26 @@ def load_notes_from_file(filename):
             value = value.strip()
             note[key] = value
             notes.append(note)
-
         # Проверяем результат
         print(notes)
-
         # Возвращаем из функции список словарей
         return notes
 
 # Проверяем есть ли нужный нам файл
 try:
-    file = open('filename.txt', encoding='utf-8')
-    file.close()
-    # Вызываем функцию
-    load_notes_from_file(file)
+    with open('filename.txt', 'r', encoding='utf-8') as filename:
+        filename.close()
+    # Вызываем функцию для дальнейшей работы с файлом
+        filename = load_notes_from_file(filename)
+# Если файл отсутствует, выводится сообщение о создании нового
 except FileNotFoundError:
-    print('Файл с таким именем не обнаружен.')
-
+    print('Файл с таким именем не обнаружен. Создан новый файл')
+    filename = open('filename.txt', 'a+', encoding='utf-8')
+    filename.close()
+    # Вызываем функцию для дальнейшей работы с файлом
+    filename = load_notes_from_file(filename)
+except UnicodeDecodeError:
+    print('Ошибка при чтении файла filename. Проверьте его содержимое.')
+except PermissionError:
+    print('Ошибка доступа')
 
