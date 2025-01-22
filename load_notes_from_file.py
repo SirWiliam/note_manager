6ad9dd0d22f70f51a913c161a4479f0a0d9ea720
SirWiliam <cloud64region@mail.ru>
2025-01-22 12:38:05 +0400

    Grade 1. Этап 4: Задание 2

--- load_notes_from_file.py
+++ load_notes_from_file.py
@@ -1,45 +1,43 @@
 
-
-
 # Создаём функцию для чтения файла и переноса содержимого в список словарей
 def load_notes_from_file(filename):
     notes = []
+
+    original_keys = {
+        'Имя пользователя': 'username',
+        'Заголовок': 'title',
+        'Описание': 'content',
+        'Статус': 'status',
+        'Дата создания': 'created_date',
+        'Дедлайн': 'issue_date'
+    }
     try:   # Открываем нужный файл с помощью конструкции with
-        with open('filename.txt', 'r', encoding='utf-8') as filename:
-            all_string = filename.readlines()
-            if len(all_string) == 0:
-                print('Открытый файл пуст')
-                return notes
-
-            symbols = ('— — —\n')
-
-            # Убираем ненужные символы за один проход
-            all_string = [line for line in all_string if line != symbols]
-
+        with open('filename.txt', 'r', encoding='utf-8') as file:
+            all_string = file.read()
+            if all_string:
+                note_string = all_string.split('\n— — —\n')
+                for string in note_string:
+                    note_lines = string.split('\n')
+                    note = {}
             # Назначаем ключ и значение с помощью разделения ":", записываем в словарь и добавляем в список
-            for i in all_string:
-                note = {}
-                key, value = i.split(':')
-                value = value.strip()
-                note[key] = value
-                notes.append(note)
+                    for line in note_lines:
+                        russian_key, value = line.split(': ', 1)
+                        key = original_keys[russian_key]
+                        value = value.strip()
+                        note[key] = value
+                    notes.append(note)
+
     except FileNotFoundError:
-        print('Файл с таким именем не обнаружен. Создан новый файл')
-        filename = open('filename.txt', 'x', encoding='utf-8')
-        filename.close()
-        return load_notes_from_file(filename='filename.txt')
+        print('Файл с таким именем не обнаружен.')
     except UnicodeDecodeError:
         print('Ошибка при чтении файла filename. Проверьте его содержимое.')
     except PermissionError:
         print('Ошибка доступа')
     except Exception as e:
-        print(f'Произошла ошибка {e}')
-
-    # Проверяем результат
-    print(notes)
+        print(f'Произошла ошибка: {e}')
     # Возвращаем из функции список словарей
     return notes
 
 if __name__=='__main__':
-
-    filename = load_notes_from_file(filename='filename.txt')
+    notes = load_notes_from_file(filename='filename.txt')
+    print(notes)
