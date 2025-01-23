a50370bb1b4a26cacc22985d8299610e4a54d5eb
SirWiliam <cloud64region@mail.ru>
2025-01-23 18:47:09 +0400

    Grade 1. Этап 3: Задание 3

--- display_notes_function.py
+++ display_notes_function.py
@@ -22,74 +22,74 @@ notes = []
 
 # Простой цикл для интерактивности
 # Создание заметок начинаем с вопроса
-while True:
-    question = input(Style.BRIGHT + 'Хотите создать заметку? (да\нет): ').lower()
+if __name__ == '__main__':
+    while True:
+        question = input(Style.BRIGHT + 'Хотите создать заметку? (да\нет): ').lower()
 
-# Если вопрос положительный, начинается сбор информации для заметки
-    if question == 'да':
-       note = {}
-       while True:
-           username = input(Style.BRIGHT + 'Введите имя пользователя: ')
-           if username == '':
-               print(Fore.RED + 'Неверный ввод. Нужно ввести хоть один символ.')
-               continue
-           else:
-               break
-       note['username'] = username
-       while True:
-           title = input(Style.BRIGHT + 'Введите название заметки: ')
-           if title == '':
-               print(Fore.RED + 'Неверный ввод. Нужно ввести хоть один символ.')
-               continue
-           else:
-               break
-       note['title'] = title
-       while True:
-           content = input(Style.BRIGHT + 'Введите описание заметки: ')
-           if content == '':
-               print(Fore.RED + 'Неверный ввод. Нужно ввести хоть один символ.')
-               continue
-           else:
-               break
-       note['content'] = content
+        # Если вопрос положительный, начинается сбор информации для заметки
+        if question == 'да':
+            note = {}
+            while True:
+                username = input(Style.BRIGHT + 'Введите имя пользователя: ')
+                if username == '':
+                    print(Fore.RED + 'Неверный ввод. Нужно ввести хоть один символ.')
+                    continue
+                else:
+                    break
+            note['username'] = username
+            while True:
+                title = input(Style.BRIGHT + 'Введите название заметки: ')
+                if title == '':
+                    print(Fore.RED + 'Неверный ввод. Нужно ввести хоть один символ.')
+                    continue
+                else:
+                    break
+            note['title'] = title
+            while True:
+                content = input(Style.BRIGHT + 'Введите описание заметки: ')
+                if content == '':
+                    print(Fore.RED + 'Неверный ввод. Нужно ввести хоть один символ.')
+                    continue
+                else:
+                    break
+            note['content'] = content
 
-# Статус для новых заметок присваивается автоматически
-       status = 'Новая'
-       note['status'] = status
+            # Статус для новых заметок присваивается автоматически
+            status = 'Новая'
+            note['status'] = status
 
-# Время создания присваивается автоматически
-       created_date = datetime.today().date()
-       note['created_date'] = datetime.strftime(created_date, '%d-%m-%Y')
-       while True:
-           issue_date = input(Style.BRIGHT + 'Пожалуйста, введите дату дедлайна\n'
-                              ' в формате "дд-мм-гггг"(через дефис и без пробелов): ')
-           try:
-               issue_date = datetime.strptime(issue_date, '%d-%m-%Y').date()
-               print(Style.BRIGHT + 'Вы ввели: ', issue_date.strftime('%d-%m-%Y'))
-           except ValueError:
-               print(Fore.RED + 'Неверный ввод! Попробуйте ещё.')
-               continue
-           if issue_date == created_date:
-               print(Fore.RED + 'Внимание! Дедлайн истекает сегодня.')
-               break
-           elif issue_date < created_date:
-               difference = created_date - issue_date
-               print(Fore.RED + 'Внимание! Дедлайн истёк: ', difference.days, 'дней назад!')
-               break
-           elif issue_date > created_date:
-               difference_1 = issue_date - created_date
-               print(Fore.RED + 'Внимание! До дедлайна: ', difference_1.days, 'дней!')
-               break
-       note['issue_date'] = datetime.strftime(issue_date, '%d-%m-%Y')
-       notes.append(note)
-       continue
-# При отрицательном ответе в список нечего не добавляется
-    elif question == 'нет':
-        break
-    else:
-        print(Fore.RED + 'Неверный ввод. Попробуйте ещё раз.')
-        continue
+            # Время создания присваивается автоматически
+            created_date = datetime.today().date()
+            note['created_date'] = datetime.strftime(created_date, '%d-%m-%Y')
+            while True:
+                issue_date = input(Style.BRIGHT + 'Пожалуйста, введите дату дедлайна\n'
+                                                  ' в формате "дд-мм-гггг"(через дефис и без пробелов): ')
+                try:
+                    issue_date = datetime.strptime(issue_date, '%d-%m-%Y').date()
+                    print(Style.BRIGHT + 'Вы ввели: ', issue_date.strftime('%d-%m-%Y'))
+                except ValueError:
+                    print(Fore.RED + 'Неверный ввод! Попробуйте ещё.')
+                    continue
+                if issue_date == created_date:
+                    print(Fore.RED + 'Внимание! Дедлайн истекает сегодня.')
+                    break
+                elif issue_date < created_date:
+                    difference = created_date - issue_date
+                    print(Fore.RED + 'Внимание! Дедлайн истёк: ', difference.days, 'дней назад!')
+                    break
+                elif issue_date > created_date:
+                    difference_1 = issue_date - created_date
+                    print(Fore.RED + 'Внимание! До дедлайна: ', difference_1.days, 'дней!')
+                    break
+            note['issue_date'] = datetime.strftime(issue_date, '%d-%m-%Y')
+            notes.append(note)
+            continue
+        # При отрицательном ответе в список нечего не добавляется
+        elif question == 'нет':
+            break
+        else:
+            print(Fore.RED + 'Неверный ввод. Попробуйте ещё раз.')
+            continue
 deinit()
 # В конце выводится функция, которая показывает созданные или нет заметки
 display_notes(notes)
-
